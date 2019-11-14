cd9eb188688adaa2c4f619785057430ee294241e
yjll1019 <yjll1019@naver.com>
2019-11-14 17:51:02 +0900

    test

--- study/src/test/java/sql/QueryTest.java
+++ study/src/test/java/sql/QueryTest.java
@@ -12,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QueryTest {
-
     private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
